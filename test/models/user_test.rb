2bf0ee80d4720de6b243910fab6287f1f5bd5e5d
msymori <msy.mori.developer@gmail.com>
2019-11-23 09:14:48 +0000

    add password model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,8 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+    password: "foobar", password_confirmation: "foobar")
   end
   
   test "should be valid" do
@@ -20,5 +21,12 @@ class UserTest < ActiveSupport::TestCase
   test "email should be present" do
     @user.email = "   "
     assert_not @user.valid?
+  end
+  
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
 end
