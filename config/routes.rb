6be283739db827639bc4949da21312172503fa2d
msymori <msy.mori.developer@gmail.com>
2019-11-24 05:50:19 +0000

    add login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root to: 'homes#index'
   get '/help', to: 'homes#help'
   get '/about', to: 'homes#about'
   get '/contact', to: 'homes#contact'
   get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
