20e89c9ac49b49f34bc3fc1c93bf0fbdd69b10c9
msymori <msy.mori.developer@gmail.com>
2019-11-30 00:17:27 +0000

    add forget password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+             email: "mori@example.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
